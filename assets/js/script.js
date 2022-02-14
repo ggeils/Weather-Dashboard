a6b0ae6da8c94524754f364c2b4db92c87ed2c3c
George Geils <95538913+ggeils@users.noreply.github.com>
2022-02-13 21:46:37 -0500

    createSearchHistory function coded to generate history if return null in localstorage for script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
 const apiKey = '8a12db2e111f4e6f3e0d6278298daa8c';
+let inputForm = $('#city');
+
+let createSearchHistory = function() {
+    let searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
+    if (searchHistory == null) {
+        searchHistory = ["Austin, Chicago, New York, Orlando, San Francisco, Seattle, Denveer, Atlanta"]
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+    }
+}
